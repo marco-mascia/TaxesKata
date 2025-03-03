1dae8df88e50d4f819c969553f6b9e25fd0b27c3
Marco Mascia <marco.mascia@gmail.com>
2025-03-03 17:13:05 +0100

    layout fixes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import './style.css'
 import { printToScreen } from './print-to-screen.ts'
 import { calculateReceipt } from './taxes-calculator.ts'
-import { pickRandomItems, setupRandomizer } from './randomizer.ts';
+import { pickRandomItems } from './randomizer.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Taxes Kata</h1>
     <div id="receipt" class="alignLeft"></div>
-    <div class="card">
-      <button id="randomizer" type="button">Randomize</button>
-    </div>
   </div>
 `
-
-setupRandomizer(document.querySelector<HTMLButtonElement>('#randomizer')!)
 
 const element = document.getElementById('receipt');
 const products = pickRandomItems(10);
--- src/print-to-screen.ts
+++ src/print-to-screen.ts
@@ -2,7 +2,7 @@ import { Product, Receipt, taxesLabel, totalLabel } from "./taxes-calculator";
 
 export const printToScreen = (receipt: Receipt) => {
 
-    console.log(receipt)
+    //console.log(receipt)
     const div = document.createElement("div");
       
     receipt.items.forEach((product: Product) => {
@@ -11,12 +11,28 @@ export const printToScreen = (receipt: Receipt) => {
         div.appendChild(p);
     });
     
+    div.appendChild(document.createElement("hr"));
+    div.appendChild(printTaxes(receipt));
+    div.appendChild(printTotal(receipt));
+    //div.appendChild(printDisclaimer());
+
+    return div;
+}
+
+export const printTaxes = (receipt: Receipt) => {
     const taxes = document.createElement("p");
     taxes.textContent = `${taxesLabel} ${receipt.totalTax.toFixed(2)}`;
-    div.appendChild(taxes);
+    return taxes;
+}
+
+export const printTotal = (receipt: Receipt) => {
     const total = document.createElement("p");
     total.textContent = `${totalLabel} ${receipt.totalAmount.toFixed(2)}`;
-    div.appendChild(total);
-  
-    return div;
+    return total;
+}
+
+export const printDisclaimer = () => {
+    const disclaimer = document.createElement("p");
+    disclaimer.textContent = `* taxed product`;
+    return disclaimer;
 }
