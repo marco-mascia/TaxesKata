8dc9dd45f76038363f1add6a003e95145e8e2ae7
Marco Mascia <marco.mascia@gmail.com>
2025-03-04 10:01:07 +0100

    fixed print test

--- src/print-to-screen.test.ts
+++ src/print-to-screen.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { printToScreen } from "./print-to-screen";
-import { ProductCategory, Receipt } from "./taxes-calculator";
+import { Receipt } from "./types";
 
 describe('Taxes Calculator', () => {
     
@@ -14,9 +14,9 @@ describe('Taxes Calculator', () => {
     it("should create a div with product details, taxes, and total", () => {
         const receipt: Receipt = {
           items: [
-            { name: "book", price: 12.49, quantity: 2, tax: 0, total: 24.98, category: ProductCategory.BOOKS},
-            { name: "music CD", price: 14.99, quantity: 1, category: ProductCategory.MUSIC, tax: 1.50, total: 24.98 },
-            { name: "spaghetti", price: 1.89, quantity: 7, tax: 0, total: 13.23, category: ProductCategory.FOOD }, 
+            {  name: "book - fiction", price: 12.49, categoryId: 3, quantity: 2, tax: 0, priceTotal: 24.98 },
+            {  name: "music CD", price: 14.99, categoryId: 5, quantity: 1, tax: 1.5, priceTotal: 16.49 },
+            {  name: "chocolate bar", price: 0.85, categoryId: 3, quantity: 1, tax: 0, priceTotal: 0.85 },
           ],
           totalTax:  1.50, 
           totalAmount: 42.32
@@ -25,9 +25,9 @@ describe('Taxes Calculator', () => {
         const div = printToScreen(receipt);
         expect(div).toBeInstanceOf(HTMLDivElement);
         expect(div.children.length).toBe(7);
-        expect(div.children[0].textContent).toBe("2 book24.98");
+        expect(div.children[0].textContent).toBe("2 book - fiction24.98");
         expect(div.children[1].textContent).toBe("1 music CD14.99*");
-        expect(div.children[2].textContent).toBe("7 spaghetti13.23");
+        expect(div.children[2].textContent).toBe("1 chocolate bar0.85");
 
         expect(div.children[4].textContent).toBe("Sales Taxes: 1.50");
         expect(div.children[5].textContent).toBe("Total: 42.32");
